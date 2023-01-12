6275a8a0614bb406d1b1dddb382458f3c479f031
AIQueen <105072501+AIqueen7@users.noreply.github.com>
2023-01-11 20:04:44 -0700

    Update facebook_scraper.py

--- facebook_scraper.py
+++ facebook_scraper.py
@@ -1,29 +1,41 @@
 import streamlit as st
 import facebook
+import requests
 
-def search_places(access_token, location, query):
+def scrape_fb_data(access_token, location, query):
     graph = facebook.GraphAPI(access_token)
+    profiles = []
     try:
         search_results = graph.request(
-            "/search",
+            "search",
             {
-                "type": "place",
+                "type": "user",
                 "q": query,
-                "fields": "name, location",
-                "center": location,
-                "distance": 1000 # search for places within 1km
+                "fields": "id,name",
+                "location": location
             }
         )
-        return search_results['data']
+        for profile in search_results["data"]:
+            profiles.append(profile["name"])
+        return profiles
     except Exception as e:
         st.error(f"An error occurred: {e}")
         return None
 
 def main():
-    st.title("Facebook Place Scraper")
+    st.title("Facebook Scraper")
 
     access_token = st.text_input("Enter your Facebook Access Token:")
-    location = st.text_input("Enter the location coordinates (lat, long):")
+    location = st.text_input("Enter the location to search for:")
     query = st.text_input("Enter the keywords to search for:")
     if st.button("Submit"):
-        places = search_places(access_token, location, query)
+        profiles = scrape_fb_data(access_token, location, query)
+        if profiles:
+            st.write("Results:")
+            for profile in profiles:
+                st.write("- " + profile)
+        else:
+            st.warning("No results found.")
+
+if __name__ == "__main__":
+    main()
